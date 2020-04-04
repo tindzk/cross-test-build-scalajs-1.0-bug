837cb625bfbc05c9515677684bf2fa8ba0984551
Tim Nieradzik <tim@sparse.tech>
2020-04-04 18:37:17 +0100

    Minimise example

--- test-project/shared/src/main/scala/hello/Hello.scala
+++ test-project/shared/src/main/scala/hello/Hello.scala
@@ -2,8 +2,6 @@ package hello
 
 object Hello {
   def greet(name: String): String = {
-    // We do this to check that environment variables work
-    Environment.requireEnvironmentVariable()
     s"Hello, $name!"
   }
 
